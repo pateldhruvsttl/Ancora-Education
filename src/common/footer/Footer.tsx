fdeb8db05663f144ed905bd4e099b1a58c411fb0
Maharshi-Parmar <maharshi.parmar@silvertouch.com>
2023-07-24 16:05:44 +0530

    make structure as a typography

--- src/common/footer/Footer.tsx
+++ src/common/footer/Footer.tsx
@@ -1,13 +1,15 @@
 
 import './Footer.scss'
 import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box/Box';
 
 function Footer() {
     return (
-        <Container maxWidth = "xl">
-            <div className='footer-container'>
-                <strong>Copyright © 2023 ANCORA. All rights reserved.</strong>
-            </div>
+        <Container maxWidth="xl">
+            <Box className='footer-container'>
+                <Typography component='strong'>Copyright © 2023 ANCORA. All rights reserved.</Typography>
+            </Box>
         </Container>
     )
 }
