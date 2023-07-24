6fe89b34de9766b7abe32345abc4d476d0bfe803
Maharshi-Parmar <maharshi.parmar@silvertouch.com>
2023-07-24 16:50:04 +0530

    sticky navbar css

--- src/common/navbar/Navbar.tsx
+++ src/common/navbar/Navbar.tsx
@@ -59,7 +59,7 @@ export default function DrawerAppBar(props: Props) {
     const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex' }} className='navbar'>
             <CssBaseline />
             <AppBar component="nav">
                 <Container maxWidth="xl">
