ec3d9d680851b0d695e1285fedb644b52bb776b3
Maharshi-Parmar <maharshi.parmar@silvertouch.com>
2023-07-24 19:28:15 +0530

    make a short code for listing
    make a short code for listing

--- src/components/accessingCMS/accessing.tsx
+++ src/components/accessingCMS/accessing.tsx
@@ -11,6 +11,7 @@ import './accessing.scss';
 import '../userprofile/userprofile.scss';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import { ListItemText } from '@mui/material';
 
 function accessing() {
 
@@ -36,6 +37,10 @@ function accessing() {
         event.preventDefault();
         console.info('You clicked a breadcrumb.');
     }
+
+    const ListData = ['Electronic Delivery', 'Traditional Mail Delivery', 'Heartland ECSI', 'Student Portal Access'];
+    const ListText = ['If you signed up for electronic delivery before January 1st 2023, then your 1098T has been sent to the email address you provided- please be sure to check your spam folder.', 'If you did not sign up for electronic delivery, then the hardcopy will be mailed to the address we have on file by January 31st.', 'provides students the ability to easily access and view their 2022 1098T forms online at: https://heartland.ecsi.net', ' 1098T Forms can also be accessed through the student portal- My Finances > 1098T- you will be directed to ECSI’s website'];
+
 
 
     return (
@@ -66,32 +71,14 @@ function accessing() {
                             </Box>
                             <Box className="contentText">
                                 <Typography component="h6"><Typography component="strong">We are pleased to announce that the 2022 1098Ts are now available!</Typography></Typography>
-                                <List className='ContentListing'>
-                                    <ListItem>
-                                        <Typography component='span'>Electronic Delivery</Typography>
-                                        <Typography component='p'>
-                                            If you signed up for electronic delivery before January 1st 2023, then your 1098T has been sent to the email address you provided- please be sure to check your spam folder.
-                                        </Typography>
-                                    </ListItem>
-                                    <ListItem>
-                                        <Typography component='span'>Traditional Mail Delivery</Typography>
-                                        <Typography component='p'>
-                                            If you did not sign up for electronic delivery, then the hardcopy will be mailed to the address we have on file by January 31st.
-                                        </Typography>
-                                    </ListItem>
-                                    <ListItem>
-                                        <Typography component='span'>Heartland ECSI</Typography>
-                                        <Typography component='p'>
-                                            provides students the ability to easily access and view their 2022 1098T forms online at: https://heartland.ecsi.net
 
-                                        </Typography>
-                                    </ListItem>
-                                    <ListItem>
-                                        <Typography component='span'>Student Portal Access</Typography>
-                                        <Typography component='p'>
-                                            1098T Forms can also be accessed through the student portal- My Finances &gt; 1098T- you will be directed to ECSI’s website
-                                        </Typography>
-                                    </ListItem>
+                                <List className='ContentListing'>
+                                    {ListData.map((ListHeading, i) => (
+                                        <ListItem key={ListHeading}>
+                                            <Typography component='span'>{ListHeading}</Typography>
+                                            <Typography component='p'>{ListText[i]}</Typography>
+                                        </ListItem>
+                                    ))}
                                 </List>
                             </Box>
                         </Box>
@@ -101,6 +88,7 @@ function accessing() {
         </>
     )
 }
+
 
 
 
