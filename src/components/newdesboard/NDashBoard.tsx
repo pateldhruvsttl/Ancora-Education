919ecee097bf64b48949c2ca1af6a44bb1726dcf
Maharshi-Parmar <maharshi.parmar@silvertouch.com>
2023-07-25 10:35:25 +0530

    make changes according to issue that create after  import global css
    make changes according to issue that create after  import global css

--- src/components/newdesboard/NDashBoard.tsx
+++ src/components/newdesboard/NDashBoard.tsx
@@ -4,6 +4,8 @@ import Footer from '../../common/footer/Footer'
 import Navbar from '../../common/navbar/Navbar'
 import '../newdesboard/Ndashboard.scss'
 import UserProfile from '../userprofile/userprofile'
+import Accessing from '../accessingCMS/accessing'
+import '../../css/global.scss'
 function Dashboard() {
 
 
