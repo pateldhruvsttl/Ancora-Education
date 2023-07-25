a7abb4b2f1b457519252d0fe37c078cf236af65d
Maharshi-Parmar <maharshi.parmar@silvertouch.com>
2023-07-25 17:51:39 +0530

    import a new page in this file

--- src/components/newdesboard/NDashBoard.tsx
+++ src/components/newdesboard/NDashBoard.tsx
@@ -6,6 +6,11 @@ import '../newdesboard/Ndashboard.scss'
 import UserProfile from '../userprofile/userprofile'
 import Accessing from '../accessingCMS/accessing'
 import '../../css/global.scss'
+import Academics from '../academic/academic'
+import Finance from '../financesAccount/finances'
+import StudentService from '../studentService/studentservice'
+import Courses from '../courses/courses'
+
 function Dashboard() {
 
 
